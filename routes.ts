a695e6537d5c48d61f3293bf9c76316c78d3b5e1
Vitor <vitor_cortinovis@outlook.com>
2021-02-27 17:02:16 -0300

    Commit send email - P1

--- routes.ts
+++ routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { SendMailController } from "./src/controllers/SendMailCOntroller";
 import { SurveysController } from "./src/controllers/SurveysController";
 import { UserController } from "./src/controllers/UserController";
 
@@ -6,9 +7,11 @@ const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
 
 router.post("/users", userController.create);
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
