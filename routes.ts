08d99d9ce6072e74e2c39b68e829304cf3cbad22
Vitor <vitor_cortinovis@outlook.com>
2021-03-03 22:08:14 -0300

    Commit final!

--- routes.ts
+++ routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { AnswerController } from "./src/controllers/AnswerController";
+import { NpsController } from "./src/controllers/NpsController";
 import { SendMailController } from "./src/controllers/SendMailController";
 import { SurveysController } from "./src/controllers/SurveysController";
 import { UserController } from "./src/controllers/UserController";
@@ -10,11 +11,13 @@ const userController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
 const answerController = new AnswerController();
+const npsController = new NpsController();
 
 router.post("/users", userController.create);
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
 router.post("/sendMail", sendMailController.execute);
 router.get("/answers/:value", answerController.execute);
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
--- src/controllers/NpsController.ts
+++ src/controllers/NpsController.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import { getCustomRepository, Not, IsNull } from "typeorm";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 
 class NpsController {
   /*
@@ -11,7 +13,28 @@ class NpsController {
   */
 
   async execute(request: Request, response: Response) {
+    const { survey_id } = request.params;
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+    const surveysUser = await surveysUsersRepository.find({
+      survey_id,
+      value: Not(IsNull())
+    });
 
+    const detractor = surveysUser.filter(survey => (survey.value >= 0 && survey.value <= 6)).length;
+    const promoters = surveysUser.filter(survey => (survey.value >= 9 && survey.value <= 10)).length;
+    const passives = surveysUser.filter(survey => (survey.value >= 7 && survey.value <= 8)).length;
+
+    const totalAnswer = surveysUser.length;
+
+    const calculate = Number((((promoters - detractor) / totalAnswer) * 100).toFixed(2));
+
+    return response.json({
+      detractor,
+      promoters,
+      passives,
+      totalAnswer,
+      nps: calculate
+    });
   }
 }
 
